1564110a4f4208bea1e22593ca508fd034c60e73
charlieahn <charlieahn@macbook-pro.attlocal.net>
2022-07-30 14:33:14 -0700

    finished

--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,48 +1,31 @@
 package _03_print_and_popups._4_quiz_game;
-
+import java.util.*;  
 import javax.swing.JOptionPane;
-
+import java.util.ArrayList;
 public class QuizGame {
 	public static void main(String[] args) {
-
-		// Create a variable to hold the user's score. Set it equal to zero.
-		int score = 0;
-		// ASK A QUESTION AND CHECK THE ANSWER
-		String adjective = JOptionPane.showInputDialog("Are the Patriots the greatest fotball team in existance?");
-		// 2. Ask the user a question
-		if (adjective.contentEquals("yes")) {
-			String prompt = "Correct";
-			JOptionPane.showMessageDialog(null, prompt);
-			score = score + 1;
-		} else {
-			String prompt = "You are kinda dumb not gonna lie";
-			JOptionPane.showMessageDialog(null, prompt);
-			score = score - 1;
-		}
-		// 3. Use an if statement to check if their answer is correct
-		String question2 = JOptionPane.showInputDialog("What is the first name of the Patriot's quaterback?");
-		// 2. Ask the user a question
-		if (question2.contentEquals("Tom")) 
-		{
-			String prompt = "Correct";
-			JOptionPane.showMessageDialog(null, prompt);
-			score = score + 1;
-		} 
-		else 
-		{
-			String prompt = "You are kinda dumb not gonna lie";
-			JOptionPane.showMessageDialog(null, prompt);
-			score = score - 1;
-		}
-		String fc = "your final score was " + score;
-		JOptionPane.showMessageDialog(null, fc);
-		
-		// 4. if the user's answer was correct, add one to their score
-
-		// MAKE MORE QUESTIONS. Ask more questions by repeating the above
-		// Option: Subtract a point from their score for a wrong answer
-
-		// After all the questions have been asked, tell the user their final score
-
+		// stage controls the stage of the setup
+		int stage = 0;
+		// when stage is zero the intro begins
+		// begin is for the start to initalize the program and explain
+		String begin = "Hello I am chatty your virtual assistant";
+		// printing chatty intro screen
+		JOptionPane.showMessageDialog(null, begin);
+		// after the user hits ok we will begin the learning process
+		String training1 = JOptionPane.showInputDialog("please input a series of texts");
+		// printing what they entered
+		JOptionPane.showMessageDialog(null, training1 + "\nprocessing. Give me a few seconds");
+		int y = 1;
+		List<String> list=new ArrayList<String>();  
+		list.add("mango");
+	    list.add("Apple");  
+		list.add("Banana");  
+		list.add("Grapes");  
+		for(String fruit:list)  
+		{e
+			  System.out.println(fruit);  
+			  
+	    }  
 	}
+
 }
